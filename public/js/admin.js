d3a83d51a39d79865cfb202f9a83296e0ef5e5e8
Alexander Novitsky <novitsky.413x@gmail.com>
2025-09-10 14:52:52 +0300

    352. Sending & Handling Background Requests

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,20 @@
 const deleteProduct = (btn) => {
     const productId = btn.parentNode.querySelector('input[name="productId"]').value;
     const csrf = btn.parentNode.querySelector('input[name="_csrf"]').value;
+
+    fetch(`/admin/product/${productId}`, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token': csrf,
+        },
+    })
+        .then((result) => {
+            return result.json();
+        })
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
